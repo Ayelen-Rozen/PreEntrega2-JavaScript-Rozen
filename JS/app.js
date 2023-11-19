f1ad0e28ed12ca496c430d9e7eba2dc01b543ec8
ayerozen <ayelenrozen90@gmail.com>
2023-11-19 16:08:02 -0300

    borro comentarios

--- JS/app.js
+++ JS/app.js
@@ -27,33 +27,3 @@ function limpiarResultado() {
 
 
 
-// function sumar() {
-//     let num1 = parseInt(document.getElementById("num1").value, 10);
-//     let num2 = parseInt(document.getElementById("num2").value, 10);
-//     let resultado = num1 + num2;
-//     document.getElementById("resultado").innerText = resultado;
-//     alert("El resultado es " + resultado);
-// }
-
-// function restar() {
-//     let num1 = parseInt(document.getElementById("num1").value, 10);
-//     let num2 = parseInt(document.getElementById("num2").value, 10);
-//     let resultado = num1 - num2;
-//     document.getElementById("resultado").innerText = resultado;
-// }
-
-// function multiplicar() {
-//     let num1 = parseInt(document.getElementById("num1").value, 10);
-//     let num2 = parseInt(document.getElementById("num2").value, 10);
-//     let resultado = num1 * num2;
-//     document.getElementById("resultado").innerText = resultado;
-// }
-
-// function dividir() {
-//     let num1 = parseInt(document.getElementById("num1").value, 10);
-//     let num2 = parseInt(document.getElementById("num2").value, 10);
-//     let resultado = num1 / num2;
-//     document.getElementById("resultado").innerText = resultado;
-// }
-
-
