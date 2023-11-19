335a0fab00b7e293548e9d942e0a9cc6957c9e55
ayerozen <ayelenrozen90@gmail.com>
2023-11-19 15:59:54 -0300

    quito formulario y agrego function limpiar resultado

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,59 @@
-function sumar() {
+const operacionesMatematicas = {
+    sumar: (a, b) => a + b,
+    restar: (a, b) => a - b,
+    multiplicar: (a, b) => a * b,
+    dividir: (a, b) => a / b,
+};
+
+function realizarOperacion(operacion) {
     let num1 = parseInt(document.getElementById("num1").value, 10);
     let num2 = parseInt(document.getElementById("num2").value, 10);
-    let resultado = num1 + num2;
-    document.getElementById("resultado").innerText = resultado;
+
+    let resultado = operacionesMatematicas[operacion](num1, num2);
+    mostrarResultado(resultado);
 }
 
-function restar() {
-    let num1 = parseInt(document.getElementById("num1").value, 10);
-    let num2 = parseInt(document.getElementById("num2").value, 10);
-    let resultado = num1 - num2;
-    document.getElementById("resultado").innerText = resultado;
+function mostrarResultado(resultado) {
+    let resultadoValor = document.getElementById("resultadoValor");
+    resultadoValor.textContent = "El resultado es: " + resultado;
+    let resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.style.display = "block";
 }
 
-function multiplicar() {
-    let num1 = parseInt(document.getElementById("num1").value, 10);
-    let num2 = parseInt(document.getElementById("num2").value, 10);
-    let resultado = num1 * num2;
-    document.getElementById("resultado").innerText = resultado;
+function limpiarResultado() {
+    let resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.style.display = "none";
 }
 
-function dividir() {
-    let num1 = parseInt(document.getElementById("num1").value, 10);
-    let num2 = parseInt(document.getElementById("num2").value, 10);
-    let resultado = num1 / num2;
-    document.getElementById("resultado").innerText = resultado;
-}
+
+
+// function sumar() {
+//     let num1 = parseInt(document.getElementById("num1").value, 10);
+//     let num2 = parseInt(document.getElementById("num2").value, 10);
+//     let resultado = num1 + num2;
+//     document.getElementById("resultado").innerText = resultado;
+//     alert("El resultado es " + resultado);
+// }
+
+// function restar() {
+//     let num1 = parseInt(document.getElementById("num1").value, 10);
+//     let num2 = parseInt(document.getElementById("num2").value, 10);
+//     let resultado = num1 - num2;
+//     document.getElementById("resultado").innerText = resultado;
+// }
+
+// function multiplicar() {
+//     let num1 = parseInt(document.getElementById("num1").value, 10);
+//     let num2 = parseInt(document.getElementById("num2").value, 10);
+//     let resultado = num1 * num2;
+//     document.getElementById("resultado").innerText = resultado;
+// }
+
+// function dividir() {
+//     let num1 = parseInt(document.getElementById("num1").value, 10);
+//     let num2 = parseInt(document.getElementById("num2").value, 10);
+//     let resultado = num1 / num2;
+//     document.getElementById("resultado").innerText = resultado;
+// }
 
 
